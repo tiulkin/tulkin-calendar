deb67b2c0538cabc585e8979d7acf6c60548cd9b
Oleg Tulkin <otulkin@stream.ru>
2020-10-02 18:28:41 +0300

    Disable warnings far demo

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,10 @@
 import 'core-js/features/object/from-entries';
+import { YellowBox } from 'react-native';
 import React from 'react';
 import { useAppComponent } from './useAppComponent';
 import { AppStateProvider } from '../components/AppStateProvider';
+
+console.disableYellowBox = true;
 
 const App: React.FC = () => {
     const AppComponent = useAppComponent();
